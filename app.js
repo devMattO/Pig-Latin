2d74c1952e77e587f39305e83dc4ffbca25b0659
Matt Olsen <olsen_matthew@yahoo.com>
2016-08-28 15:00:28 -1000

    cleaning up code

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ let checkForFirstVowel = ( input, vowels ) => {
     for (let j = 0; j < vowels.length; j++) {
       if(vowels[j] === input[i]){
         let nonVowels = arr.splice(0,(counter-1));
-        arr = arr.concat(nonVowels,'ay');
+        arr = arr.concat(`-${nonVowels.join('')}ay`);
         return arr.join('');
       }
     }
@@ -29,37 +29,10 @@ let checkForFirstVowel = ( input, vowels ) => {
 let pigLatinTranslator = input => {
   let vowels = ['a','e','i','o','u'];
   let translation = input.split('');
-  let completedTranslation;
-  completedTranslation = checkForBeginningVowels(translation,vowels);
-  // completedTranslation = checkForFirstVowel(translation,vowels);
-  return completedTranslation;
+  let completedTranslation1 = checkForBeginningVowels(translation,vowels);
+  let completedTranslation2 = checkForFirstVowel(translation,vowels);
+  return completedTranslation1,completedTranslation2;
 };
 
-console.log(pigLatinTranslator("ate"), 'outside func');
+console.log(pigLatinTranslator("handsome"), 'outside func');
 
-
-
-// function pigLatinTranslator( input ) {
-//   var inputArr = input.split(' ');
-//   var pigLatinWords = [];
-//   var vowels = [ 'a', 'e', 'i', 'o', 'u' ];
-//   var eachWord;
-//   for( var i = 0; i < inputArr.length; i++ ){
-//     eachWord = inputArr[i].toString();
-//     var newStr = eachWord.slice(0,1);
-//     eachWord.substring(1);
-//     console.log(newStr, '<---', eachWord.substring(1));
-//     for (var j = 0; j < vowels.length; j++){
-//       if( eachWord[0] === vowels[j] ){
-//         inputArr[i].slice(0,1);
-//         pigLatinWords.push(inputArr[i] + 'ay');
-//         console.log(pigLatinWords);
-//       } else {
-//         // console.log(pigLatinWords.push(eachWord + firstLetter + 'ay'));
-//       }
-//     }
-//   }
-//   // console.log(pigLatinWords);
-// }
-
-// pigLatinTranslator('banana apples surfboards');
